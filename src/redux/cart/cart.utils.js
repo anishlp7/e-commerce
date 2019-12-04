c4f380aed0f17f008a56b8210b50069d34dc62fb
Anish <39819327+anishlp7@users.noreply.github.com>
2019-12-04 12:58:08 +0530

    Added Checkout componenet and also remove functionality,quanity increase,decrese added-Redux Dnctionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,18 @@ export const addItemToCart = (cartItems,cartItemsToAdd) => {
 
     return [...cartItems,{...cartItemsToAdd, quantity:1}]
 }
+
+export const removeItemFromCart = (cartItems,removeItemFromCart) => {
+    const existingItem = cartItems.find(cartItem => cartItem.id === removeItemFromCart.id);
+
+    if(existingItem === 1){
+        return cartItems.filter(cartItem => cartItem.id !== removeItemFromCart.id);
+    }
+
+    return cartItems.map( cartItem => cartItem.id === removeItemFromCart.id
+         ?
+            { ...cartItem, quantity: cartItem.quantity - 1 }
+          :
+           cartItem  
+        )
+}
