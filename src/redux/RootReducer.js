0e8199abc2f33d1c4df867e101eb561f7632daf6
Anish <39819327+anishlp7@users.noreply.github.com>
2019-12-04 13:46:47 +0530

    Redux Persist Functionaliy is implemented and executed

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,9 +1,23 @@
 import {  combineReducers } from 'redux';
 
+import { persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage'
+
 import UserReducer from './User/UserReducer';
 import CartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persisConfig = {
+    key:'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user:UserReducer,
     cart:CartReducer
 })
+
+
+
+export default persistReducer(persisConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import RootReducer from './RootReducer';
 
 const middleware = [logger];
 
-const store = createStore(RootReducer,applyMiddleware(...middleware));
+export const store = createStore(RootReducer,applyMiddleware(...middleware));
 
-export default store;
+export const persistor = persistStore(store)
